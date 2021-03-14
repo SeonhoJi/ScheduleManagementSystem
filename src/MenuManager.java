2602658e27cb8a8253e3c33bc5432b2b4aac91a2
SeonhoJi <tjsghss5318@naver.com>
2021-03-14 23:10:29 +0900

    removed Hello,java and added MenuManager

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Schedule");
+		System.out.println("2. Delete Schedule");
+		System.out.println("3. Edit Schedule");
+		System.out.println("4. View Schedule");
+		System.out.println("5. Exit");
+	}
+
+}
