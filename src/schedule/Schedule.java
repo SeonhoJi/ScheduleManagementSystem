2ecf7ac8f97a013003429d038d81afe21162be6a
SeonhoJi <tjsghss5318@naver.com>
2021-04-12 21:26:12 +0900

    added ScheduleKind getUserInput

--- src/schedule/Schedule.java
+++ src/schedule/Schedule.java
@@ -1,9 +1,44 @@
-package ScheduleManagementSystem;
+package schedule;
+
+import java.util.Scanner;
 
 public class Schedule {
-	String place;
-	int date;
-	String business;
+	protected ScheduleKind kind = ScheduleKind.light;
+	protected String place;
+	protected int date;
+	protected String business;
+	
+	public ScheduleKind getKind() {
+		return kind;
+	}
+
+	public void setKind(ScheduleKind kind) {
+		this.kind = kind;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public int getDate() {
+		return date;
+	}
+
+	public void setDate(int date) {
+		this.date = date;
+	}
+
+	public String getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(String business) {
+		this.business = business;
+	}
 	
 	public Schedule() {
 		
@@ -18,6 +53,18 @@ public class Schedule {
 	public void printInfo() {
 		System.out.println("place : "+ place + " date : " + date + " business : " + business );
 	}
-
-
+	
+	public void getUserInput(Scanner input) {
+		System.out.print("Appointed date");
+		int date = input.nextInt();
+		this.setDate(date);
+		
+		System.out.print("Meeting place");
+		String place = input.next();
+		this.setPlace(place);
+		
+		System.out.print("The business");
+		String business = input.next();
+		this.setBusiness(business);
+	}
 }
