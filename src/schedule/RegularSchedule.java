4e47b3351300fd0e7724a4865f841763a4265ccb
SeonhoJi <tjsghss5318@naver.com>
2021-05-14 20:10:08 +0900

    created new class

--- src/schedule/RegularSchedule.java
+++ src/schedule/RegularSchedule.java
@@ -2,38 +2,21 @@ package schedule;
 
 import java.util.Scanner;
 
-public class RegularSchedule extends Schedule implements Scheduleinput {
+public class RegularSchedule extends Schedule{
 	
 	public RegularSchedule(ScheduleKind kind) {
 		super(kind);
 	}
 	
 	public void getUserInput(Scanner input) {
-		System.out.print("Appointed date");
-		int date = input.nextInt();
-		this.setDate(date);
+		setDate(input);
 		
-		System.out.print("Meeting place");
-		String place = input.next();
-		this.setPlace(place);
+		setPlace(input);
 		
-		System.out.print("The business");
-		String business = input.next();
-		this.setBusiness(business);
+		setBusiness(input);
 	}
 	public void printInfo() {
-		String skind = "none";
-		switch(this.kind) {
-		case regular:
-			skind = "regular";
-			break;
-		case important:
-			skind = "important";
-			break;
-		case hangout:
-			skind = "hangout";
-			break;
-		}
+		String skind = getKindString();
 		System.out.println("kind : " + skind + "place : "+ place + " date : " + date + " business : " + business );
 	}
 }
