144844a9349bc0299715bd4557571e880ca0f826
SeonhoJi <tjsghss5318@naver.com>
2021-05-14 20:11:40 +0900

    refactoring

--- src/ScheduleManagementSystem/MenuManager.java
+++ src/ScheduleManagementSystem/MenuManager.java
@@ -1,5 +1,6 @@
 package ScheduleManagementSystem;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuManager {
@@ -8,36 +9,48 @@ public class MenuManager {
 		Scanner input = new Scanner(System.in);
 		ScheduleManager scheduleManager = new ScheduleManager(input);
 		
+		selectMenu(input, scheduleManager);
+	}
+	public static void selectMenu(Scanner input, ScheduleManager scheduleManager) {
 		int num = 0;
-		
 		while(num!=5) {
-			System.out.println("*** Schedule Management System Menu***");
-			System.out.println("1. Add Schedule");
-			System.out.println("2. Delete Schedule");
-			System.out.println("3. Edit Schedule");
-			System.out.println("4. View Schedules");
-			System.out.println("5. Exit");
-			System.out.println("Select one number between 1-5 : ");
-			num = input.nextInt();
-			
-			
-			if(num==1) {
-				scheduleManager.addSchedule();
+			try {
+				showMenu();
+				num = input.nextInt();
+				switch(num) {
+				case 1:
+					scheduleManager.addSchedule();
+					break;
+				case 2:
+					scheduleManager.deleteSchedule();
+					break;
+				case 3:
+					scheduleManager.editSchedule();
+					break;
+				case 4:
+					scheduleManager.veiwSchedules();
+					break;
+				default:
+					continue;
+				}
 			}
-			
-			else if(num==2) {
-				scheduleManager.deleteSchedule();
-			}
-			
-			else if(num==3) {
-				scheduleManager.editSchedule();
-			}
-			
-			else if(num==4) {
-				scheduleManager.veiwSchedules();
+			catch(InputMismatchException e){
+				System.out.println("Please put an integer between 1 and 4 !");
+				if(input.hasNext()) {
+					input.next();
+				}
+				num = -1;
 			}
 		}
-		
 	}
 	
+	public static void showMenu() {
+		System.out.println("*** Schedule Management System Menu***");
+		System.out.println("1. Add Schedule");
+		System.out.println("2. Delete Schedule");
+		System.out.println("3. Edit Schedule");
+		System.out.println("4. View Schedules");
+		System.out.println("5. Exit");
+		System.out.println("Select one number between 1-5 : ");
+	}
 }
