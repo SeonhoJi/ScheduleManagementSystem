cb90046e24cd9e528d8a71ac501671bd527e3c57
SeonhoJi <tjsghss5318@naver.com>
2021-05-14 20:07:41 +0900

    .

--- src/schedule/ImportantSchedule.java
+++ src/schedule/ImportantSchedule.java
@@ -2,53 +2,19 @@ package schedule;
 
 import java.util.Scanner;
 
-public class ImportantSchedule extends Schedule implements Scheduleinput {
+public class ImportantSchedule extends NoRegularSchedule{
 	
 	public ImportantSchedule(ScheduleKind kind) {
 		super(kind);
 	}
 	
 	public void getUserInput(Scanner input) {
-		System.out.print("Appointed date");
-		int date = input.nextInt();
-		this.setDate(date);
-		
-		char answer = 'x';
-		while(answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N') {
-			System.out.print("Do you know where to meet? (Y/N)");
-			answer = input.next().charAt(0);
-			if (answer == 'Y' || answer =='y') {
-				System.out.print("Meeting place");
-				String place = input.next();
-				this.setPlace(place);
-				break;
-			}
-			else if (answer == 'Y' || answer =='y') {
-				this.setPlace("");
-				break;
-			}
-			else {
-				
-			}
-		}
-
-		System.out.print("The business");
-		String business = input.next();
-		this.setBusiness(business);
+		setDate(input);
+		setPlacewithYN(input);
+		setBusinesswithYN(input);
 	}
 	public void printInfo() {
-		String skind = "none";
-		switch(this.kind) {
-		case regular:
-			skind = "regular";
-			break;
-		case important:
-			skind = "important";
-			break;
-		case hangout:
-			skind = "hangout";
-			break;
-		}
+		String skind = getKindString();
 		System.out.println("kind : " + skind + "place : "+ place + " date : " + date + " business : " + business );
 	}
 }
