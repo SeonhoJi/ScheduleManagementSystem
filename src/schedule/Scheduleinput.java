1be27f627bbe10b19d16a1faf6d9f451b64c41e3
SeonhoJi <tjsghss5318@naver.com>
2021-05-14 20:11:16 +0900

    added exception

--- src/schedule/Scheduleinput.java
+++ src/schedule/Scheduleinput.java
@@ -2,6 +2,8 @@ package schedule;
 
 import java.util.Scanner;
 
+import exception.PlaceFormatException;
+
 public interface Scheduleinput {
 	
 	
@@ -11,9 +13,15 @@ public interface Scheduleinput {
 	
 	public void setBusiness(String business);
 	
-	public void setPlace(String place);
+	public void setPlace(String place) throws PlaceFormatException;
 	
 	public void getUserInput(Scanner input);
 	
 	public void printInfo();
+	
+	public void setDate(Scanner input);
+	
+	public void setPlace(Scanner input);
+	
+	public void setBusiness(Scanner input);
 }
