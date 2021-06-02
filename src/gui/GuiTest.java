261736b47f4725e97bd307329747a137be23a923
SeonhoJi <tjsghss5318@naver.com>
2021-06-02 15:36:59 +0900

    GUI add, view

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -3,9 +3,10 @@ package gui;
 public class GuiTest {
 
 	public static void main(String[] args) {
-		//MenuSelection menuselection = new MenuSelection();
-		//ScheduleAdder scheduleadder = new ScheduleAdder();
-		ScheduleViewer scheduleviewer = new ScheduleViewer();
+		//WindowFrame frame = new WindowFrame();
+		
+		
+		
 	}
 
 }
